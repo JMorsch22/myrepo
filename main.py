326b8c397359da5d13727bd58e5e402bcf6c72f5
JMorsch22 <jack.morsches@gmail.com>
2020-05-05 06:41:10 -0400

    cron file

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, request, escape
 from flask import jsonify
 #from flask_jsonpify import jsonpify
 import yfinance as yf
-from pandas_datareader import data as pdr
+#from pandas_datareader import data as pdr
 #import pandas as pd
-from injest_stock_data import *
+from ingest_stock_data import *
 
 
 app = Flask(__name__)
@@ -15,6 +15,13 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello Jack, you changed this locally!'
 
+@app.route('/update/<value>')
+def update(value):
+    """Loads historical stock data into bucket."""
+    ticker = value
+    bucket = "tax-loss-harvesting.appspot.com"
+    ingest(ticker,bucket)
+    return ticker + ' data loading to ' + bucket
 
 @app.route('/name/<value>')
 def name(value):
@@ -35,13 +42,36 @@ def ohlc(value):
     return jsonify(val)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 # [END gae_python37_app]
+'''
 @app.route('/params',method = ['POST'])
 def ingest_ohlc_data():
     json = request.get_json()
 
-    ticker = escape(json['ticker']) if 'ticker' in json else None
-    bucket = escape(json['bucket'])  # required
+    ticker = escape(json["ticker"]) # required
+    bucket = escape(json["bucket"]) # required
 
     gcsfile = ingest(ticker, bucket)
+
+@app.route('/test',method = ['POST'])
+def ingest_ohlc_data():
+    json = request.get_json()
+    #ticker = escape(json["ticker"]) # required
+    #bucket = escape(json["bucket"]) # required
+
+    #gcsfile = ingest(ticker, bucket)
+    #ticker = yf.Ticker(ticker)
+    #val = ticker.history(period='1d')
+    #val = {"close": val.Close.values[0]}
+    #print(val)
+    return json
+
+@app.route('/uplate/<value>')
+def update_stock(value):
+    ticker = value
+    #bucket = "tax-loss-harvesting.appspot.com"
+    #ingest(ticker,bucket)
+    #json = {ticker+'uploaded to '+bucket}
+    return 'this is an update function '+ticker #jsonify(json)
+'''
